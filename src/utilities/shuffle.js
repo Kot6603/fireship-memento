e0124a197d9b448ad6015e447fd663a24dd5c413
kot <89110272+Kot6603@users.noreply.github.com>
2023-12-29 19:17:58 +1300

    fixed the shuffling algorithm

--- src/utilities/shuffle.js
+++ src/utilities/shuffle.js
@@ -12,9 +12,19 @@ const shuffle = () => {
     { image: '/assets/ts.png' },
   ]
 
-  return [...assets, ...assets]
+  const fisherYates = arr => {
+    for (let i = arr.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [arr[i], arr[j]] = [arr[j], arr[i]];
+    }
+    return arr
+  }
+
+  const combinedArr = [...assets, ...assets]
     .map((card) => ({ ...card, id: Math.random() }))
     .sort(() => Math.random - 0.5)
+
+  return fisherYates(combinedArr)
 }
 
 export default shuffle
